72fdf6eb425f1f605a8eee067d33eed51950518c
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-11-23 15:24:31 +0000

    typescript format

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ import { Experience } from "./components/Experience";
 function App() {
   return (
     <>
-      <Canvas camera={{ position: [0, 0.5, 5], fov: 42 }}>
-        <color attach="background" args={["#f5f3ee"]} />
+      <Canvas camera={ { position: [0, 0.5, 5], fov: 42 } }>
+        <color attach="background" args={ ["#f5f3ee"] } />
         <fog attach="fog" args={ ["#f5f3ee", 10, 50] } />
         <ScrollControls
           pages={ config.sections.length }
           damping={ 0.1 }
           maxSpeed={ 0.2 }
         >
-          <group position-y={-1}>
+          <group position-y={ -1 }>
             <Experience />
           </group>
         </ScrollControls>
