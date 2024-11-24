341f255cba73746253029bb80514269244f78b92
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-11-24 00:12:37 +0000

    Staging - Home

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,14 @@
 import { useRef } from "react";
-import { Environment, useScroll } from "@react-three/drei";
+import { Environment, useScroll, Center, Float } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { Avatar } from "./Avatar";
 import { SectionTitle } from "./SectionTitle";
+import { MacBookPro } from "./MacBookPro";
+import { PalmTree } from "./PalmTree";
+import { Star } from "./Star";
+
+import { config } from "../config";
+import * as THREE from "three";
 
 const SECTIONS_DISTANCE = 10;
 
@@ -23,7 +29,46 @@ export const Experience = () => {
       <group ref={ sceneContainer }>
         {/* HOME */ }
         <group>
-          <SectionTitle position-x={ 0.5 }>HOME</SectionTitle>
+          <Star position-z={ 0 } position-y={ 2.2 } scale={ 0.3 } />
+          <Float floatIntensity={ 2 } speed={ 2 }>
+            <MacBookPro
+              position-x={ -1 }
+              position-y={ 0.5 }
+              position-z={ 0 }
+              scale={ 0.3 }
+              rotation-y={ Math.PI / 4 }
+            />
+          </Float>
+          <PalmTree
+            scale={ 0.018 }
+            rotation-y={ THREE.MathUtils.degToRad(140) }
+            position={ [4, 0, -5] }
+          />
+          <Float floatIntensity={ 0.6 }>
+            <Center disableY disableZ>
+              <SectionTitle
+                size={ 0.8 }
+                position-y={ 1.6 }
+                position-z={ -3 }
+                bevelEnabled
+                bevelThickness={ 0.3 }
+              >
+                { config.home.title }
+              </SectionTitle>
+            </Center>
+          </Float>
+          <Center disableY disableZ>
+            <SectionTitle
+              size={ 0.8 }
+              position-x={ -1 }
+              position-z={ -3 }
+              bevelEnabled
+              bevelThickness={ 0.3 }
+              rotation-y={ Math.PI / 10 }
+            >
+              { config.home.subtitle }
+            </SectionTitle>
+          </Center>
         </group>
         {/* SKILLS */ }
         <group position-z={ SECTIONS_DISTANCE }>
