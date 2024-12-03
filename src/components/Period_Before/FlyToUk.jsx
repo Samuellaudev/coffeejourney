fcd478f448c9b680925dd27e96817b91e90594cc
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-12-03 20:35:47 +0000

    Added ExpandableText to FlyToUk and Jobless components

--- src/components/Period_Before/FlyToUk.jsx
+++ src/components/Period_Before/FlyToUk.jsx
@@ -3,40 +3,54 @@ import { SectionTitle } from '../SectionTitle';
 import { Airplane } from './Airplane';
 import { Flag } from './Flag';
 import { AvatarSophie } from '../AvatarSophie';
+import { ExpandableText } from '../ExpandableText';
 
 export const FlyToUk = () => {
   return (
     <>
-      {/* Group containing the SectionTitle and Flag */}
-      <group position-x={-2.1}>
+      {/* Group containing the SectionTitle and Flag */ }
+      <group position-x={ -2.1 }>
         <SectionTitle
-          position-x={-0.4}
-          position-y={0.2}
-          position-z={1.5}
-          rotation-y={Math.PI / 6}
+          position-x={ -0.4 }
+          position-y={ 0.2 }
+          position-z={ 1.5 }
+          rotation-y={ Math.PI / 6 }
           color="#7586d9"
         >
           Flight To UK
         </SectionTitle>
 
-        <group rotation-y={Math.PI / 6}>
-          <Flag scale={0.6} position-y={0.6} />
-          <RoundedBox scale={[1.5, 0.5, 1]} position-y={0.3} position-z={-1}>
+        <group rotation-y={ Math.PI / 6 }>
+          <Flag scale={ 0.6 } position-y={ 0.6 } />
+          <RoundedBox scale={ [1.5, 0.5, 1] } position-y={ 0.3 } position-z={ -1 }>
             <meshStandardMaterial color="white" />
           </RoundedBox>
         </group>
       </group>
 
-      <AvatarSophie position-x={1} />
+      <AvatarSophie position-x={ 1 } />
 
-      {/* Floating Airplane */}
-      <Float floatIntensity={1.5} speed={2}>
+      {/* Floating Airplane */ }
+      <Float floatIntensity={ 1.5 } speed={ 2 }>
         <Airplane
-          position={[2, 1.8, -0.5]}
-          rotation-y={Math.PI / 6}
-          scale={0.03}
+          position={ [2, 1.8, -0.5] }
+          rotation-y={ Math.PI / 6 }
+          scale={ 0.03 }
         />
       </Float>
+
+      <ExpandableText
+        content="Due to the political events that took place in Hong Kong in 2019, I decided to migrate to the UK with my wife on a BNO (British National (Overseas)) visa, which allows both of us to work, live, and start a new life here legally!"
+        typingSpeed={ 20 }
+        maxLength={ 37 }
+        textSize={ 0.09 }
+        initialPosition={ [0.1, 1, 0] }
+        smallTextPosition={ [0.1, 0.9, 0] }
+        planeInitialSize={ [3.5, 0.6] }
+        planeExpandedSize={ [2, 0.5] }
+        groupPosition={ [2.3, 0, 0] }
+        expandedTextLength={ 2 }
+      />
     </>
   );
 };
--- src/components/Period_Before/Jobless.jsx
+++ src/components/Period_Before/Jobless.jsx
@@ -5,52 +5,66 @@ import { BookCase } from '../Period_During/BookCase';
 import { CouchSmall } from '../Period_During/CouchSmall';
 import { Lamp } from '../Period_During/Lamp';
 import { EmojiSad } from '../Period_Before/EmojiSad';
+import { ExpandableText } from '../ExpandableText';
 
 export const Jobless = () => {
   return (
     <>
-      {/* Left Group - Floating Emoji, Bookcase, Couch, and Lamp */}
-      <group position={[-2, 0, -1]}>
-        <Float floatIntensity={0.6}>
-          <EmojiSad position-y={3.3} rotation-y={0.2} />
+      {/* Left Group - Floating Emoji, Bookcase, Couch, and Lamp */ }
+      <group position={ [-2, 0, -1] }>
+        <Float floatIntensity={ 0.6 }>
+          <EmojiSad position-y={ 3.3 } rotation-y={ 0.2 } />
         </Float>
 
-        <BookCase position-z={-2} />
+        <BookCase position-z={ -2 } />
 
-        <CouchSmall 
-          scale={0.4} 
-          position={[ -0.2, 0, 0 ]} 
-          rotation-y={Math.PI / 3} 
+        <CouchSmall
+          scale={ 0.4 }
+          position={ [-0.2, 0, 0] }
+          rotation-y={ Math.PI / 3 }
         />
 
-        <Lamp 
-          position={[ -0.4, -0.8, 0.6 ]} 
-          rotation-y={-Math.PI} 
+        <Lamp
+          position={ [-0.4, -0.8, 0.6] }
+          rotation-y={ -Math.PI }
+        />
+
+        <ExpandableText
+          content="However, things didn’t go as planned. It's not easy to find a job in the UK if I don’t have any local work experience, which eventually led to a one-year employment break (sounds more positive than being called jobless T_T)"
+          typingSpeed={ 20 }
+          maxLength={ 37 }
+          textSize={ 0.09 }
+          groupPosition={ [0.3, -0.4, 1] }
+          initialPosition={ [0.1, 1, 0] }
+          smallTextPosition={ [0.1, 0.9, 0] }
+          planeInitialSize={ [3.5, 0.6] }
+          planeExpandedSize={ [2, 0.5] }
+          expandedTextLength={ 2 }
         />
       </group>
 
-      {/* Right Group - Dynamic and Static Section Titles */}
-      <group position={[0.1, 0, -1]} rotation-y={-0.3}>
-        <Float floatIntensity={0.6}>
-          <SectionTitle 
-            size={0.7} 
-            position={[ 0.1, 2.3, -3 ]} 
-            color="yellow" 
-            bevelEnabled 
-            bevelThickness={0.3}
+      {/* Right Group - Dynamic and Static Section Titles */ }
+      <group position={ [0.1, 0, -1] } rotation-y={ -0.3 }>
+        <Float floatIntensity={ 0.6 }>
+          <SectionTitle
+            size={ 0.7 }
+            position={ [0.1, 2.3, -3] }
+            color="yellow"
+            bevelEnabled
+            bevelThickness={ 0.3 }
           >
-            {config.jobless.dynamicTitle}
+            { config.jobless.dynamicTitle }
           </SectionTitle>
         </Float>
 
-        <SectionTitle 
-          size={0.5} 
-          position={[ 0, 1.2, -3 ]} 
-          rotation-y={-0.2} 
-          bevelEnabled 
-          bevelThickness={0.3}
+        <SectionTitle
+          size={ 0.5 }
+          position={ [0, 1.2, -3] }
+          rotation-y={ -0.2 }
+          bevelEnabled
+          bevelThickness={ 0.3 }
         >
-          {config.jobless.staticTitle}
+          { config.jobless.staticTitle }
         </SectionTitle>
       </group>
     </>
