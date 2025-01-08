338f34f72ad8b51fa742efa82720bf6ea0db7df1
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2025-01-08 18:04:59 +0000

    Modified scale and position of items in WorkPlacement component

--- src/components/Period_During/WorkPlacement.jsx
+++ src/components/Period_During/WorkPlacement.jsx
@@ -1,3 +1,4 @@
+import { useMobile } from '../../hooks/useMobile';
 import { Float } from '@react-three/drei';
 import { SectionTitle } from '../SectionTitle';
 import { CustomText } from '../CustomText';
@@ -9,10 +10,14 @@ import { Arrow } from './Arrow';
 import { ExpandableText } from '../ExpandableText';
 
 export const WorkPlacement = () => {
+  const { isMobile, scaleFactor } = useMobile()
+
   return (
     <>
       {/* Left group for titles and coffee shop image */ }
-      <group position={ [-2, 0, 0] }>
+      <group
+        position={ isMobile ? [-1.7, -0, -4] : [-2, 0, 0] }
+      >
         <group scale={ 1 } position={ [-0.2, -0.2, 1.5] } rotation-y={ Math.PI / 6 }>
           <SectionTitle position-y={ 0.7 } color="#508182">
             Work
@@ -34,20 +39,23 @@ export const WorkPlacement = () => {
         </Float>
         <ExpandableText
           content="In the second half of the program, I had the chance to complete a six-day work placement, each shift lasting six hours. I'm so grateful for the opportunity—it really helped level up my skills as a barista. The hands-on experience gave me a chance to apply everything I learned in real-life situations, boosting my confidence and refining my craft."
-          typingSpeed={ 20 }
+          typingSpeed={ 15 }
           maxLength={ 37 }
-          textSize={ 0.09 }
-          groupPosition={ [1, 0.2, 1] }
           initialPosition={ [0.1, 1, 0] }
           smallTextPosition={ [0.1, 0.9, 0] }
+          textSize={ isMobile ? 0.069 : 0.09 }
+          groupPosition={ isMobile ? [1.6, -0.7, 8] : [1, 0.2, 1] }
           planeInitialSize={ [3.3, 0.6] }
-          planeExpandedSize={ [2, 0.6] }
-          expandedTextLength={ 2 }
+          planeExpandedSize={ isMobile ? [1.5, 0.5] : [2, 0.5] }
+          expandedTextLength={ isMobile ? 1.5 : 2 }
         />
       </group>
 
       {/* Right group for floating photos and labeled arrows */ }
-      <group position={ [0, 0, 0] }>
+      <group
+        scale={ isMobile ? 0.8 : 1 }
+        position={ isMobile ? [0, 0.2, -4] : [0, 0, 0] }
+      >
         <Float floatIntensity={ 1 } speed={ 1.5 }>
           <PhotoBoxEight />
         </Float>
